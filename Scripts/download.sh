9672071c39ab2db551c8f260e5c0fa1966175d48
Steve Kite <steve@actor.im>
2015-10-06 08:20:37 +0300

    Renamed pod spec file

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -4,6 +4,8 @@ set -ev
 j2objc_version=0.9.8.2.1
 sha1_checksum=ba24a3cbae4f21e5f33f26b2db2bc3d4568cb198
 
+rm -fr Frameworks
+
 echo "fetching j2objc.framework"
 curl -OL https://github.com/actorapp/J2ObjC-Framework/releases/download/v${j2objc_version}/j2objc.framework.zip
 echo "${sha1_checksum}  j2objc.framework.zip" | shasum -c
