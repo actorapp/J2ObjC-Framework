9c1fa43d21284d0917f4d41d347f51470041224c
Gijs van Veen <gijs@splendo.com>
2016-10-26 11:41:27 +0200

    Fixed checksum

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -2,7 +2,7 @@
 set -ev
 
 j2objc_version=1.2
-sha1_checksum=e25a4a6eb57155cbb6ece2c316aed67ccabef599
+sha1_checksum=62988333bcf505514a1af8aa37ee00c5f2b59c35
 
 echo "fetching j2objc dist"
 curl -OL https://github.com/google/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
